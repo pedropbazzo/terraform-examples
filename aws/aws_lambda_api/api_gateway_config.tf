b27426437b7f0d88f1114f49f06029a746082a2f
Pedro Paulo Bazzo <pedropbazzo@outlook.com>
2021-03-02 05:00:23 -0300

    ---

--- aws/aws_lambda_api/api_gateway_config.tf
+++ aws/aws_lambda_api/api_gateway_config.tf
@@ -6,7 +6,7 @@ resource "aws_api_gateway_rest_api" "this" {
 resource "aws_api_gateway_deployment" "this" {
   rest_api_id = "${aws_api_gateway_rest_api.this.id}"
 
-  depends_on = [
+  depends_on = [ 
     "aws_api_gateway_integration.proxy_root",
     "aws_api_gateway_integration.proxy_other",
   ]
